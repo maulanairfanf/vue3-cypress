fef0703ef70195646fe35121f4099d65ff87c46c
maulanairfanf <maulanairfanf@gmail.com>
2025-02-11 09:32:10 +0700

    fix ci

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  component: {
-    devServer: {
-      framework: "vue",
-      bundler: "vite",
-    },
-  },
+	e2e: {
+		baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:5173',
+	},
+	component: {
+		devServer: {
+			framework: 'vue',
+			bundler: 'vite',
+		},
+	},
 
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+	e2e: {
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+		},
+	},
+})
